b374885dbdfa554751126f830d8eda726ef4dc46
Nate Christianson <NateChristianson@Nates-MacBook-Air.local>
2017-04-04 14:46:10 -0600

    DW-2 implementing User flow, importing user from redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import stateTest from 'darity-state';
 
 class App extends Component {
   render() {
@@ -8,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{stateTest}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
