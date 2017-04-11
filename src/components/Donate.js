99f0a0863761b681186aa60009679e2e0d7bfe2d
Liza Sautter <lizasautter@gmail.com>
2017-04-11 11:10:21 -0600

    DW-14: creation of splash page

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import {User} from 'darity-state';
+import 'whatwg-fetch';
 
 export default class Donate extends Component {
 
   render() {
+    // NEEDS CSS CHANGES
     return (
     <div>
       <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
@@ -15,14 +17,9 @@ export default class Donate extends Component {
             <option value="Scrooge Option #2">Scrooge Option #2 $10.00 USD</option>
             <option value="Santa Option #3">Santa Option #3 $20.00 USD</option>
         </select> </td></tr>
-        <tr><td><input type="hidden" className="on1" value="Test" />Test</td></tr><tr><td><select className="os1">
-            <option value="Option 1">Option 1 </option>
-            <option value="Option 2">Option 2 </option>
-            <option value="Option 3">Option 3 </option>
-        </select> </td></tr>
         </table>
         <input type="hidden" className="currency_code" value="USD" />
-        <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" className="submit" alt="PayPal - The safer, easier way to pay online!" />
+        <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" className="submit" />
         <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
       </form>
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,70 @@
 import React, { Component } from 'react';
-import {User} from 'darity-state';
-import 'whatwg-fetch';
+import '../App.css';
 
 class Login extends Component {
-  state = {
-            username: '', 
-            password: ''
-          }
-
-  loginSubmit(){
-    fetch('/authenticate', {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json' //content type in mobile = accept
-      },
-      body: JSON.stringify({
-        username: this.state.username,
-        password: this.state.password
-      })
-    }).then((user) => {
-      this.props.loginAction(user.username, user.token)
-      if (this.props.token) {
-        return "You are now logged in.";
-      } else { 
-        return user.message
-      }
-
-    })
-  }
-
   render() {
+  	console.log("pop")
     return (
-      <div className='Login'>
-        <input type='text' placeholder='Username' value={this.state.username} onChange={(username) => this.setState({username})} />
-        <input type='password' placeholder='Password' value={this.state.password} onChange={(password) => this.setState({password})} />
-        <button className='loginButton' type="submit" onSubmit={this.loginSubmit.bind(this)}>Login</button> 
+      <div className='textAreaBox'>
+	      <div>
+	      <p><span className='login'><a href='#'>Login</a></span>or <span className='signUp'><a href='#'>Sign Up</a></span></p>  
+	      </div>
+	      <div className="continue">
+	      <p><a href='#'>Continue To Site</a></p>
+	      </div>
       </div>
     );
   }
 }
+// https://www.npmjs.com/package/react-player
+export default Login;
 
-export default User(Login);
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import {User} from 'darity-state';
+// import 'whatwg-fetch';
+
+// class Login extends Component {
+//   state = {
+//             username: '', 
+//             password: ''
+//           }
+
+//   loginSubmit(){
+//     fetch('/authenticate', {
+//       method: 'POST', 
+//       headers: {
+//         'Content-Type': 'application/json' //content type in mobile = accept
+//       },
+//       body: JSON.stringify({
+//         username: this.state.username,
+//         password: this.state.password
+//       })
+//     }).then((user) => {
+//       this.props.loginAction(user.username, user.token)
+//       if (this.props.token) {
+//         return "You are now logged in.";
+//       } else { 
+//         return user.message
+//       }
+
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className='Login'>
+//         <input type='text' placeholder='Username' value={this.state.username} onChange={(username) => this.setState({username})} />
+//         <input type='password' placeholder='Password' value={this.state.password} onChange={(password) => this.setState({password})} />
+//         <button className='loginButton' type="submit" onSubmit={this.loginSubmit.bind(this)}>Login</button> 
+//       </div>
+//     );
+//   }
+// }
+
+// export default User(Login);
