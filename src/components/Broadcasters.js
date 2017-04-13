1fb1c9540900e0953a211e0ff08d90ecdf5263e8
g-crow <genevieve.crow@gmail.com>
2017-04-13 10:36:53 -0600

    added classnames and css to broadcaster component

--- src/components/Broadcasters.js
+++ src/components/Broadcasters.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import '../css/app.css';
+import '../css/broadcaster.css';
 
 class Broadcasters extends Component {
   render() {
   	var self = this;
     var username = 'Mary';
-    var bio = "This is a bio";
+    var bio = "This is a free online calculator which counts the number of characters or letters in a text, useful for your tweets on Twitter, as well as a multitude of other applications. And how many characters you use.";
     return (
-      <div className="Broadcaster">
+      <div className="Broadcaster-bio-container">
         <div>
-          <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSO1KwKx8bQGfbD1g1wfCL6253bZNHGko-TPEUdpmvBzBjiDg-Wfw" alt="" height="200px" width="200px"/>
+          <img className="bio-photo" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSO1KwKx8bQGfbD1g1wfCL6253bZNHGko-TPEUdpmvBzBjiDg-Wfw" alt="" height="200px" width="200px"/>
           <h1>{username}</h1>
-          <p>{bio}</p>
+          <p className="broadcaster-bio">{bio}</p>
         </div>
     </div>
     );
