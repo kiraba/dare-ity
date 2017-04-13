e6233ac54bfe98d459568187a264ce794e5dcb37
Liza Sautter <lizasautter@gmail.com>
2017-04-13 11:08:38 -0600

    DW-16 broadcaster container css updates

--- src/components/Broadcasters.js
+++ src/components/Broadcasters.js
@@ -4,17 +4,19 @@ import '../css/broadcaster.css';
 class Broadcasters extends Component {
   render() {
   	var self = this;
-    var username = 'Mary';
+    var username = '@USERNAME';
     var bio = "This is a free online calculator which counts the number of characters or letters in a text, useful for your tweets on Twitter, as well as a multitude of other applications. And how many characters you use.";
     return (
       <div className="Broadcaster-bio-container">
         <div>
-          <img className="bio-photo" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSO1KwKx8bQGfbD1g1wfCL6253bZNHGko-TPEUdpmvBzBjiDg-Wfw" alt="" height="200px" width="200px"/>
-          <h1>{username}</h1>
-          <p className="broadcaster-bio">{bio}</p>
+          <img className="bio-photo" src="http://bsoft.fr/story/wp-content/uploads/2016/09/about-thumbnail-placeholder.png" alt="Profile Picture"/>
+          <h1 className="broadcasterUsername">{username}</h1>
+          <p className="broadcaster-bio">{bio}</p> 
         </div>
     </div>
     );
   }
 }
 export default Broadcasters;
+
+//bio limit 200 characters
