4e09f8e98104d1eef49cbd58a41425eca8499c80
Nate Christianson <NateChristianson@Nates-MacBook-Air.local>
2017-04-18 17:23:16 -0600

    upload picture stuff

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      pageMode: 'Homepage'
+      pageMode: 'SplashPage'
     }
   }
 
--- src/components/signUp.js
+++ src/components/signUp.js
@@ -2,38 +2,49 @@ import React, { Component } from 'react';
 import '../css/app.css';
 import {User} from 'darity-state';
 import 'whatwg-fetch';
-
+import profilePic from '../../public/images/blank-profile-picture';
 
 class SignUp extends Component {
     state = {
-            username: '', 
+            name: '', 
             email: '',
-            password: ''
+            password: '', 
+            is_npo: false, 
+            file: {profilePic}
           }
 
   usernameChange(e){
-    this.setState({username: e.target.value})
+    this.setState({name: e.target.value})
   }
+
   emailChange(e){
     this.setState({email: e.target.value})
   }
+
   passwordChange(e){
     this.setState({password: e.target.value})
   }
 
+  npoChange(e){
+    this.setState({is_npo: e.target.boolean})
+  }
+
+
   registerSubmit(){
-    fetch('/users', {
+    fetch('/create_user', {
       method: 'POST', 
       headers: {
         'Content-Type': 'application/json' //content type in mobile = accept
       },
       body: JSON.stringify({
-        username: this.state.username,
+        name: this.state.name,
         email: this.state.email,
-        password: this.state.password
+        password: this.state.password, 
+        is_npo: this.state.is_npo, 
+        file: this.state.file
       })
     }).then((user) => {
-      this.props.registerAction(user.username, user.email, user.token)
+      this.props.registerAction(user.name, user.email, user.is_npo, user.file, user.token)
       if (this.props.token) {
         return "Your account has been registered.";
       } else { 
@@ -42,22 +53,73 @@ class SignUp extends Component {
 
     })
   }
+  
+  uploadFile(file, signedRequest, url){
+    const xhr = new XMLHttpRequest();
+    xhr.open('PUT', signedRequest);
+    xhr.onreadystatechange = () => {
+      if(xhr.readyState === 4){
+        if(xhr.status === 200){
+          document.getElementById('preview').src = url;
+          document.getElementById('avatar-url').value = url;
+        }
+        else{
+          alert('Could not upload file.');
+        }
+      }
+    };
+    xhr.send(file);
+  }
+
+  getSignedRequest(file){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+    xhr.onreadystatechange = () => {
+      if(xhr.readyState === 4){
+        if(xhr.status === 200){
+          const response = JSON.parse(xhr.responseText);
+          this.uploadFile(file, response.signedRequest, response.url);
+        }
+        else{
+          alert('Could not get signed URL.');
+        }
+      }
+    };
+    xhr.send();
+  }
+
+  imageChange() {
+    document.getElementById("file-input").onchange = () => {
+      const files = document.getElementById('file-input').files;
+      const file = files[0];
+      if(file == null){
+        return alert('No file selected.');
+      }
+      this.getSignedRequest(file);
+    }
+  }
+
   render() {
     console.log('We are getting here')
     return (
       <div className='textAreaBox'>
 	      <div>
         <p>Sign Up</p>
-        <input type='text' placeholder='Username' value={this.state.username} onChange={(username) => this.setState({username})} /> <br />
+        <input type='text' placeholder='Username' value={this.state.name} onChange={(name) => this.setState({name})} /> <br />
         <input type='text' placeholder='Email Address' value={this.state.email} onChange={(email) => this.setState({email})} /> <br />
         <input type='password' placeholder='Password' value={this.state.password} onChange={(password) => this.setState({password})} /> <br />
+        <input type='checkbox' value={this.state.is_npo} onClick={this.npoChange.bind(this)} /> <br />
+          <label for="isNpo">Check if you are a Nonprofit Organization</label>
+        <p id="status">Please upload a profile picture</p>
+        <input type="file" id="file-input" onChange={this.imageChange.bind(this)} /> <br />
+        <input type="hidden" id="avatar-url" name="avatar-url" value={profilePic}/>
         <button className='registerButton' type="submit" onSubmit={this.registerSubmit.bind(this)}>Sign Up</button> 
       </div>
       </div>
     );
   }
 }
-// https://www.npmjs.com/package/react-player
+
 export default SignUp;
 
 
