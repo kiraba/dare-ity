d08e75e4550bdb55183308963c56e8024fcfe078
Nate Christianson <NateChristianson@Nates-MacBook-Air.local>
2017-04-21 13:21:35 -0600

    confirmed reg and login functionality and storage

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import '../css/app.css';
 
 class Login extends Component {
@@ -16,54 +16,58 @@ class Login extends Component {
   }
 }
 // https://www.npmjs.com/package/react-player
-export default Login;
+export default Login;*/
 
 
+import React, { Component } from 'react';
+import {User} from 'darity-state';
+import 'whatwg-fetch';
+import '../css/app.css';
 
+class Login extends Component {
+  state = {
+            name: '', 
+            password: ''
+          }
 
+changePageMode(){
+    this.props.changePageMode('Homepage')
+  }
 
+  loginSubmit(){
+    fetch('http://fun-d-backend.herokuapp.com/api/authenticate', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json' //content type in mobile = accept
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        password: this.state.password
+      })
+    })
+    .then(response=>response.json())
+    .then((user) => {
+      this.props.login(user.name, user.token)
+      if (user.token) {
+        return this.changePageMode();
+      } else { 
+        return user.message
+      }
+    })
+  }
 
+  render() {
+    return (
+      <div className='textAreaBox'>
+        <div>
+        <p>Login</p>
+          <input type='text' placeholder='Name' value={this.state.name} onChange={(e) => this.setState({name: e.target.value})} /> <br />
+          <input type='password' placeholder='Password' value={this.state.password} onChange={(e) => this.setState({password: e.target.value})} /> <br />
+          <button className='signUpButton' type="submit" onClick={this.loginSubmit.bind(this)}>Login</button> 
+        </div>
+      </div> 
+    );
+  }
+}
 
-// import React, { Component } from 'react';
-// import {User} from 'darity-state';
-// import 'whatwg-fetch';
-
-// class Login extends Component {
-//   state = {
-//             username: '', 
-//             password: ''
-//           }
-
-//   loginSubmit(){
-//     fetch('/authenticate', {
-//       method: 'POST', 
-//       headers: {
-//         'Content-Type': 'application/json' //content type in mobile = accept
-//       },
-//       body: JSON.stringify({
-//         username: this.state.username,
-//         password: this.state.password
-//       })
-//     }).then((user) => {
-//       this.props.loginAction(user.username, user.token)
-//       if (this.props.token) {
-//         return "You are now logged in.";
-//       } else { 
-//         return user.message
-//       }
-
-//     })
-//   }
-
-//   render() {
-//     return (
-//       <div className='Login'>
-//         <input type='text' placeholder='Username' value={this.state.username} onChange={(username) => this.setState({username})} />
-//         <input type='password' placeholder='Password' value={this.state.password} onChange={(password) => this.setState({password})} />
-//         <button className='loginButton' type="submit" onSubmit={this.loginSubmit.bind(this)}>Login</button> 
-//       </div>
-//     );
-//   }
-// }
-
-// export default User(Login);
+export default User(Login);
