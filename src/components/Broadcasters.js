2460e083782546ad8d1300848cc9194764e34a5b
Liza Sautter <lizasautter@gmail.com>
2017-04-22 17:24:10 -0600

    DW-19 Broadcasters drawing from backend

--- src/components/Broadcasters.js
+++ src/components/Broadcasters.js
@@ -1,38 +1,30 @@
 import React, { Component } from 'react';
 import '../css/tile.css';
+import BroadcasterComp from './BroadcasterComp'
 
 class Broadcasters extends Component {
+  state = {
+          userBlocks: []
+        }
+
+
+  componentDidMount() {
+    fetch('http://fun-d-backend.herokuapp.com/api/fetch_all_users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json' //content type in mobile = accept
+      }
+    })
+  .then(response=>response.json())
+  .then((user) => {
+    this.setState({userBlocks: user.result});
+    console.log(user.result)
+  })
+  }
   render() {
     return (
     <div className="TilesContainer">
-        <div className="Tile BroadcastersOverlay">
-            <a className="TileLink" href="#">
-            <img src='https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/13537702_10209939099652898_8907788068611790849_n.jpg?oh=51fffe601ea84eb5ebf68f2997ff8956&oe=598447C4' />
-            <h1>@username</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt purus eget nunc ...</p>
-            </a>
-        </div>
-            <div className="Tile BroadcastersOverlay">
-            <a className="TileLink" href="#">
-            <img src='https://avatars3.githubusercontent.com/u/23302593?v=3&s=400' />
-            <h1>@username</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt purus eget nunc ...</p>
-            </a>
-        </div>
-        <div className="Tile BroadcastersOverlay">
-            <a className="TileLink" href="#">
-            <img src='https://avatars2.githubusercontent.com/u/25285076?v=3&s=400' />
-            <h1>@username</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt purus eget nunc ...</p>
-            </a>
-        </div>
-        <div className="Tile BroadcastersOverlay">
-            <a className="TileLink" href="#">
-            <img src='https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/14359152_703694599807096_6910453967565416651_n.jpg?oh=67657b76ba58c6c4b4cd64ac15355c0c&oe=597C887C' />
-            <h1>@username</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt purus eget nunc ...</p>
-            </a>
-        </div>
+    {this.state.userBlocks.map((element, i) => (<BroadcasterComp element = {element} key={i} />))}
     </div>
     );
   }
@@ -40,7 +32,3 @@ class Broadcasters extends Component {
 export default Broadcasters;
 
 // 89 characters.
-
-
-
-
