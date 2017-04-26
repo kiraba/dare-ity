36c789bb12217cbfb16978f829d04c8ea766efec
Nate Christianson <NateChristianson@Nates-MacBook-Air.local>
2017-04-26 13:32:47 -0600

    completed agreeance to do da dare

--- src/components/Participate.js
+++ src/components/Participate.js
@@ -14,6 +14,7 @@ changePageMode(){
   }
 
   participate(){
+    console.log(this.props)
     fetch('http://fun-d-backend.herokuapp.com/api/set_user_dare', {
       method: 'POST',
       headers: {
@@ -22,12 +23,13 @@ changePageMode(){
       body: JSON.stringify({
         broadcaster_id: this.props.id,
   			dare_id: this.props.currentDare.id,
-  			npo_id: this.props.currentDare.npo_id,
+  			npo_id: this.props.currentDare.npo_creator,
   			pledge_amount_threshold: this.state.threshold,
-        token: this.props.token,
-				video_path: null,
+        token: this.props.token
+				//video_path: null,
       })
     })
+    //.then(response=>response.json())
     .then(result=>result.status === 200 ? this.changePageMode() : alert("please try again"))
   }
 
@@ -39,7 +41,9 @@ changePageMode(){
         <div>
         <p>Agreement of Participation</p>
         <p className='Participate'>Amount required to complete dare? <input type='number' min='1' step='any' placeholder='$0.00' value={this.state.threshold} onChange={(e) => this.setState({threshold: e.target.value})} /></p>
-          <div><button className='loginButton' type="submit" onClick={this.participate.bind(this)}>Agree</button> <button className='loginButton' type="submit" onClick={()=>self.props.changePageMode('Homepage')}>Cancel</button></div>
+          <div>
+          <button className='loginButton' type="submit" onClick={this.participate.bind(this)}>Agree</button>  <button className='loginButton' type="submit" onClick={()=>self.props.changePageMode('Homepage')}>Cancel</button>
+          </div>
         </div>
       </div>
       </div>
